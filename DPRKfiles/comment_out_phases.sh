a5b7d101fcfba6e26bba2ec8a49ead3243d909e8
Steven J. Gibbons <74380921+stevenjgibbons@users.noreply.github.com>
2024-03-18 11:55:20 +0100

    Add files via upload

---
+++ DPRKfiles/comment_out_phases.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+commentoutfile=exclude.txt
+origfile=DPRK_clean_allCC.txt.good
+outfile=DPRK_clean_allCC.txt
+badfile=bad.txt
+if test ! -r ${commentoutfile}
+then
+  echo "No file ${commentoutfile} found"
+  exit 0
+fi
+if test ! -r ${origfile}
+then
+  echo "No file ${origfile} found"
+  exit 0
+fi
+sort ${origfile} > sorted.txt
+if test -r ${badfile}
+then
+  rm ${badfile}
+fi
+touch ${badfile}
+while read line
+do
+  set $line
+  stat=$1
+  phas=$2
+  ev=$3
+  awk '($1 == EV || $2 == EV ) && $5 == STAT && $6 == PHASE' EV=$ev STAT=$stat PHASE=$phas sorted.txt > ${badfile}
+done < ${commentoutfile}
+sort ${badfile} > bad_sorted.txt
+diff sorted.txt bad_sorted.txt | grep -v "\," | sed 's_< __g' > diff.txt
+mv diff.txt ${outfile}
